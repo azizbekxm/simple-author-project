e9d192195a3b20b2670861ee3e69404e5b365d73
Azizbek Khushvakov <azizbek_khushvakov@epam.com>
2025-01-23 21:46:35 +0500

    initial commit

---
+++ src/main/java/com/example/author_finder/AuthorFinderApplication.java
@@ -0,0 +1,13 @@
+package com.example.author_finder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthorFinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthorFinderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/author_finder/AuthorFinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.author_finder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthorFinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
